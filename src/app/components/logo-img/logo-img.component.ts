5d8d248a6e6bb1c5b30c58baf190051d98343a7d
Vladislav Setchin <vlad@setchinfc.com.au>
2021-05-14 16:23:16 +1000

    Added log image component

---
+++ src/app/components/logo-img/logo-img.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logo-img',
+  templateUrl: './logo-img.component.html',
+  styleUrls: ['./logo-img.component.scss']
+})
+export class LogoImgComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

4528837f6b5dfe7dd36fdfc29570e5b46c29d261
Vladislav Setchin <vlad@setchinfc.com.au>
2021-05-14 16:23:56 +1000

    set background and logo to header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LogoImgComponent } from './components/logo-img/logo-img.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    LogoImgComponent
   ],
   imports: [
     BrowserModule,

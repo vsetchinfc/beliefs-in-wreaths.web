afddaabc6d34b755ba8cda3c73187e666aeeb120
Vlad <vlad.setchin@gmail.com>
2021-07-02 12:07:16 +1000

    Set colours

--- apps/beliefs-in-wreaths/src/app/app.component.ts
+++ apps/beliefs-in-wreaths/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'b-in-w-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'beliefs-in-wreaths';
